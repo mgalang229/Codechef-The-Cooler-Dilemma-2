76d6804feaea9412a591cb06c0679b8a177f4a86
jasonpogi <51401355+mgalang229@users.noreply.github.com>
2023-08-05 09:20:28 +0800

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+//change class to 'Main'
+public class Solution {
+
+	public static void main(String[] args) {
+		Scanner fs = new Scanner(System.in);
+		int T = 1;
+		T = fs.nextInt();
+		for (int tc = 1; tc <= T; tc++) {
+			int x = fs.nextInt(), y = fs.nextInt();
+			int f = y / x;
+			if (x * f == y) {
+				f--;
+			}
+			System.out.println(f);
+		}
+		fs.close();
+	}
+}
